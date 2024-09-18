09e92bb864c8709e848f8171f53d8b6da9a00ed2
AyushSama <aayush0029@gmail.com>
2024-09-18 12:25:19 +0530

    Added Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component , signal } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, MatButtonToggleModule, MatCheckbox, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'PostBlog';
+  isLoggedIn = false;
+  hideSingleSelectionIndicator = signal(false);
+
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+    {
+        path : 'login',
+        component: LoginComponent,
+    },
+    {
+        path : 'feed',
+        component: FeedComponent,
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+    }
+];
